a4fbe98b1880d76b3e0c54e7bc770e029e3fb32b
linahabita <habita.lina@univ-ouargla.dz>
2024-06-28 09:50:14 -0700

    done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
-#!/usr/bin/pup
-# install puppet-lint -v 2.5.0
-
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+# Install Flask from pip3
+exec { 'install python packages':
+  command => 'pip3 install flask==2.1.0',
+  path    => ['/usr/bin/'],
+  unless  => '/usr/bin/test -f /usr/local/lib/python3.4/dist-packages/flask/app.py',
 }
+
