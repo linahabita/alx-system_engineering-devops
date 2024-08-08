a581ff7d0430f3b887fb980c717b7b5a4b65dcb0
lina habita <habita.lina@univ-ouargla.dz>
2024-08-08 13:39:34 +0100

    1

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,35 +1,32 @@
 #!/usr/bin/python3
 """
-Returns to-do list information for a given employee ID.
-
-This script takes an employee ID as a command-line argument and fetches
-the corresponding user information and to-do list from the JSONPlaceholder API.
-It then prints the tasks completed by the employee.
+this module is for expermenting a fake API
 """
-
 import requests
 import sys
 
-
 if __name__ == "__main__":
-    # Base URL for the JSONPlaceholder API
-    url = "https://jsonplaceholder.typicode.com/"
 
-    # Get the employee information using the provided employee ID
-    employee_id = sys.argv[1]
-    user = requests.get(url + "users/{}".format(employee_id)).json()
+    employee_id = int(sys.argv[1])
 
-    # Get the to-do list for the employee using the provided employee ID
-    params = {"userId": employee_id}
-    todos = requests.get(url + "todos", params).json()
+    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
 
-    # Filter completed tasks and count them
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    response = requests.get(url)
+    response_json = response.json()
+    employee_name = response_json['name']
 
-    # Print the employee's name and the number of completed tasks
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
+    todos_url = (
+        f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+    )
+    todos_response = requests.get(todos_url)
+    todos_data = todos_response.json()
+    total_tasks = len(todos_data)
+    completed_tasks = [task for task in todos_data if task['completed']]
+    done_tasks = len(completed_tasks)
 
-    # Print the completed tasks one by one with indentation
-    [print("\t {}".format(complete)) for complete in completed]
-
+    print(
+        f"Employee {employee_name} is done with tasks("
+        f"{done_tasks}/{total_tasks}):"
+    )
+    for task in completed_tasks:
+        print(f"\t {task['title']}")
